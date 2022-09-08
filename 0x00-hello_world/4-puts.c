1352b9557884df328f1490a36a592ba893802328
FionaG26 <fionamuthoni18@gmail.com>
2022-09-08 15:40:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 /**
+ * main - print the string in the put function
  *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
+ * Description: using the main function
  * * this program prints "Programming is like building a multilingual puzzle
  *
  * * Return: 0
