dfa3a2b34bdda21b751937cbb0d3bf56cb5d78cf
FionaG26 <fionamuthoni18@gmail.com>
2022-09-08 16:34:11 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main(void) 
+int main(void)	
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
