9f934a4daa6aeb08ada019e1cc84f04db757401c
FionaG26 <fionamuthoni18@gmail.com>
2022-09-09 07:44:02 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - print if the number is positive, zero, or negative
  *
- *Description: using the main function
+ * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
