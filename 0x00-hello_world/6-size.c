7d508422056cbec73cc324446116f9f073dd1e7f
FionaG26 <fionamuthoni18@gmail.com>
2022-09-08 17:31:29 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main -entry
  *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
  * Return: 0 after printf
  */
 int main(void)
