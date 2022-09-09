288034970356db0d29a07cb07539be5d9f3c62cd
FionaG26 <fionamuthoni18@gmail.com>
2022-09-09 10:22:05 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
  * main - Print the alphabet in lowercase letters, except for e & q
+ *
+ * Description: using the main function
+ * this program prints "Programming is alphabet in lowercase letters, except for e & q
  * Return: 0
- * /
+ */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	if (ch != 'e' && ch != 'q')
+if (ch != 'e' && ch != 'q')
 {
 	putchar(ch);
 }
