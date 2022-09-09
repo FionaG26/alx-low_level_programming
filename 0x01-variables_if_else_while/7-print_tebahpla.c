a63a4e77900583a5163e767761ac02b6c9f7fef3
FionaG26 <fionamuthoni18@gmail.com>
2022-09-09 12:32:41 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Description: using the putchar function
+ * this program prints "Programming is in lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet ;
+
+	for (alphabet = 122 ; alphabet >=97; alphabet--)
+
+	{
+		putchar(alphabet);
+	}
+
+putchar('\n');
+return (0);
+}
