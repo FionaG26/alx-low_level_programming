d4beddbc0900011e60648b63a6f23c15f30ae2e2
FionaG26 <fionamuthoni18@gmail.com>
2022-09-11 11:49:17 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 /*
  * File: 10-print_comb2.c
  * Auth: Fiona Githaiga
- */ 
+ */
 #include <stdio.h>
 
 /**
@@ -18,17 +18,15 @@ int main(void)
 	{
 		for (digit2 = 1; digit2 <= 9; digit2++)
 	{
-			if (digit1>digit2)
+			if (digit1 > digit2)
 	{
-				putchar(digit1+ '0')
-				putchar(digit2+ '0')
+				putchar(digit1 + '0')
+				putchar(digit2 + '0')
 				
-				if(digit 1!=8)
-				
-				
-				putchar(',');
-				putchar(' ');
-		}
+				if(digit 1! = 8)
+					putchar (',')
+					putchar (' ');
+	}
 	}
 	putchar('\n');
 	return (0);
