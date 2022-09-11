89432bbbafcab62f166722ecca7b4d17b53e5820
FionaG26 <fionamuthoni18@gmail.com>
2022-09-11 08:23:08 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,14 @@ int main(void)
 	
 	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{		
-				putchar((digit1 % 10) + '0');
+		for (digit2 = 1; digit2 <= 9; digit2++)
+	{
+			if (digit1>digit2)
+	{
+				putchar(digit1+ '0')
+				putchar(digit2+ '0')
 				
-				putchar((digit2 % 10) + '0');
-				
-				if (digit1 == 8 && digit2 == 9)
-				
-				continue 
+				if(digit 1!=8)
 				
 				
 				putchar(',');
