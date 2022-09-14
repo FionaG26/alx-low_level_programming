6bac106f0d99249bb11b2534aaf53c4bffe3ce26
FionaG26 <fionamuthoni18@gmail.com>
2022-09-14 13:49:13 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - function to check for a sign of a number
  * @n: is the int that will use for the argument of the function
- * Return : 0
+ * Return: 0
  */
 int print_sign(int n)
 {
@@ -10,7 +10,8 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}
+}
+
 
 	else if (n < 0)
 	{
