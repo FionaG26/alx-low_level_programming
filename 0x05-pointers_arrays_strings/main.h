8a51f00246ab9d6828423222dbf352e3c94ed554
FionaG26 <fionamuthoni18@gmail.com>
2022-09-19 00:36:40 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,48 @@
+/**
+ * _putchar
+ */
+int _putchar(char c);
+/**
+ * Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ */
+void reset_to_98(int *n);
+/**
+ * Write a function that swaps the values of two integers.
+ */
+void swap_int(int *a, int *b);
+/**
+ * Write a function that returns the length of a string.
+ */
+int _strlen(char *s);
+/**
+ * Write a function that prints a string, followed by a new line, to stdout.
+ */
+void _puts(char *str);
+/**
+ * Write a function that prints a string, in reverse, followed by a new line.
+ */
+void print_rev(char *s);
+/**
+ * Write a function that reverses a string.
+ */
+void rev_string(char *s);
+/**
+ * Write a function that prints one char out of 2 of a string, followed by a new line.
+ */
+void puts2(char *str);
+/**
+ * Write a function that prints half of a string, followed by a new line.
+ */
+void puts_half(char *str);
+/**
+ * Write a function that prints n elements of an array of integers, followed by a new line.
+ */
+void print_array(int *a, int n);
+/**
+ * Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
+ */
+char *_strcpy(char *dest, char *src);
+/**
+ * Write a function that convert a string to an integer.
+ */
+int _atoi(char *s);
