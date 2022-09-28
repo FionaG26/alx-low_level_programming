f866e76a2d694eedebad79ba8caed29b4d5e46af
FionaG26 <fionamuthoni18@gmail.com>
2022-09-28 03:53:48 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return lenght of a string.
+ * @x: integer base.
+ * @y: integer power to.
+ *
+ * Return: result of num1 power to num2.
+ */
+int _pow_recursion(int x, int y)
+{
+	/* base case */
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
+}
