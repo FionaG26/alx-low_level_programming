ffe4de0030248fa60cab5c9411d1ebc8ca67e407
FionaG26 <fionamuthoni18@gmail.com>
2022-09-30 00:24:06 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Program that prints its name, followed by a new line.
+* @argc: Number of command lines arguments.
+* @argv: Character of arrays.
+*
+* Return: 0.
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i = 0;
+
+	while (*(argv[0] + i) != '\0')
+	{
+		putchar(*(argv[0] + i));
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
