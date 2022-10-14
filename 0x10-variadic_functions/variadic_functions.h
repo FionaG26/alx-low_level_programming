304b869c4917bc0c87aba8d5ed59f4f38babebf6
FionaG26 <fionamuthoni18@gmail.com>
2022-10-14 10:55:40 -0700

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,23 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
-#include <stdarg.h>
-/*
- * struct check - paramenters to check against
- * @c: type
- * @f: the function to send it to
- */
-typedef struct check
-{
-	char *chk;
-	void (*f)(va_list list);
-} check_t;
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
