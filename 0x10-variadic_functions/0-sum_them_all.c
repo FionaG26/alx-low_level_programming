2497d7443200ba0fee87e879268aa6192020f16c
FionaG26 <fionamuthoni18@gmail.com>
2022-10-14 10:30:49 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,17 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	unsigned int i;
 	va_list valist;
+	unsigned int sum = 0, i;
+
+	if (n == 0)
+		return (0);
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
+
 	va_end(valist);
 
 	return (sum);
