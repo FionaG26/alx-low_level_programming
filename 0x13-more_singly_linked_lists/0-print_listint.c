0388a5319873b1e25aa8138271fdaf687805f79d
FionaG26 <fionamuthoni18@gmail.com>
2022-10-26 21:42:23 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
 * print_listint - prints all elements of a list
 *
 * @h: list to print elements of
 * Return: size_t of the list
-	    */
+*/
+
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
