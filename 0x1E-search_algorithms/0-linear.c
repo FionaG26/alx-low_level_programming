ee7237395e34c7cebab40150c3b77a0051afbc01
FionaG26 <fionamuthoni18@gmail.com>
2023-04-11 06:00:30 -0700

    tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL) {
+        return -1;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
+
+    return -1;
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*function that searches for a value in an array using the linear search algorithm*/
+int linear_search(int *array, size_t size, int value);
+/*function that searches for a value in a sorted array of integers using the binary search algorithm*/
+int binary_search(int *array, size_t size, int value);
+/*function that searches for a value in a sorted array of integers using the Jump search algorithm*/
+int jump_search(int *array, size_t size, int value);
+/*function that searches for a value in a sorted array of integers using the Interpolation search algorithm*/
+int interpolation_search(int *array, size_t size, int value);
+/*function that searches for a value in a sorted array of integers using the Exponential search algorithm*/
+int exponential_search(int *array, size_t size, int value);
+/*function that searches for a value in a sorted array of integers*/
+int advanced_binary(int *array, size_t size, int value);
+
