ccd3d9980ca02dbe976eb165e4e0ceebf5f7c667
FionaG26 <fionamuthoni18@gmail.com>
2023-04-12 02:09:19 -0700

    tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h> /* for size_t */
 
 /*function that searches for a value in an array using the linear search algorithm*/
 int linear_search(int *array, size_t size, int value);
