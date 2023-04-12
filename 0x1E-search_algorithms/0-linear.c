f17b5e0803e6d9e12cfba4b56586e29bd386c2c3
FionaG26 <fionamuthoni18@gmail.com>
2023-04-12 04:14:51 -0700

    tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,17 +12,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-int i;
 
-if (array == NULL)
-return (-1);
+	int i;
 
-for (i = 0; i < (int)size; i++)
-{
-printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-if (value == array[i])
+	if (array == NULL)
+		return (-1);
 
-return (i);
-}
-return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
 }
